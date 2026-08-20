676a5f355dcfc0a86b6847fe437b97580d0d6978
Bhanu <68260461+atabzo@users.noreply.github.com>
2026-08-20 15:18:33 +0530

    adding the barebones and yummy css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,20 @@
 <script lang="ts">
-  import DotField from '$lib/components/svelte-bits/DotField.svelte';
-  import LiquidChrome from '$lib/components/svelte-bits/LiquidChrome.svelte';
 
 </script>
 
 
-<div style="width: 100%; height: 600px; position: relative;">
-  <LiquidChrome
-    baseColor={[1, 0.1, 0.1]}
-    speed={0.2}
-    amplitude={0.5}
-    interactive={true}
-  />
-</div>
+<main class="content">
+  <h1> peptune: an indie creative studio</h1>
+  <p> coming soon!</p>
+</main>
+
+<style>
+  .content {
+    font-family: 'CMU Serif';
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 30vh;
+  }
+</style>
